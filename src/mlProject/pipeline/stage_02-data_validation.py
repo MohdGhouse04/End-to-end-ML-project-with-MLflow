0f8674301161e12cf282686008b959db2cf5cadd
MohdGhouse04 <imailghouse@gmail.com>
2026-03-13 05:47:39 +0530

    data transformation added

--- src/mlProject/pipeline/stage_02-data_validation.py
+++ src/mlProject/pipeline/stage_02-data_validation.py
@@ -1,7 +1,6 @@
 from mlProject.config.configuration import ConfigurationManager
-from mlProject.components.data_validation import DataValiadtion
+from mlProject.components.data_validation import DataValidation
 from mlProject import logger
-
 
 STAGE_NAME = "Data Validation stage"
 
@@ -12,20 +11,16 @@ class DataValidationTrainingPipeline:
     def main(self):
         config = ConfigurationManager()
         data_validation_config = config.get_data_validation_config()
-        data_validation = DataValiadtion(config=data_validation_config)
+
+        data_validation = DataValidation(config=data_validation_config)
         data_validation.validate_all_columns()
 
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataValidationTrainingPipeline()
         obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
     except Exception as e:
         logger.exception(e)
         raise e
-
